4608b4f1bdd12b40028890fadde91a3f3ce196ec
peter.goodman <peter.goodman@3c0eaa4a-8e4f-0410-92f7-c37bf31e9ff0>
2008-06-13 23:04:32 +0000

    that was odd.

--- system/pql/__init__.php
+++ system/pql/__init__.php
@@ -6,9 +6,12 @@
 
 $dir = dirname(__FILE__);
 
+// bring in the Pinq Query Language, a.k.a. PINQ: PHP Integrated Query. Yes, I
+// know that it is also the name of the framework, I just really liked it!
 require_once $dir .'/abstract-relation.php';
 require_once $dir .'/abstract-model.php';
 require_once $dir .'/abstract-query.php';
 require_once $dir .'/abstract-predicates.php';
 require_once $dir .'/concrete-query.php';
 require_once $dir .'/model-dictionary.php';
+
