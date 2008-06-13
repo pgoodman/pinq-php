b0c8cce1bda8dd973d3043e4edb91be6c86f2839
peter.goodman <peter.goodman@3c0eaa4a-8e4f-0410-92f7-c37bf31e9ff0>
2008-06-13 22:29:57 +0000

    changed the way that pql builds queries. separated predicate handling into its own class and extended that to make queries that only have a single source easier to build by using a restricted predicates class.

--- trunk/system/packages/pql/__init__.php
+++ trunk/system/packages/pql/__init__.php
@@ -7,5 +7,6 @@
 require_once DIR_PINQ .'/abstract-relation.php';
 require_once DIR_PINQ .'/abstract-model.php';
 require_once DIR_PINQ .'/abstract-query.php';
+require_once DIR_PINQ .'/abstract-predicates.php';
 require_once DIR_PINQ .'/concrete-query.php';
 require_once DIR_PINQ .'/model-dictionary.php';
