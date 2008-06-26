489e9fe04e672bb7cfd311a2cec529e93017a852
peter.goodman <peter.goodman@3c0eaa4a-8e4f-0410-92f7-c37bf31e9ff0>
2008-06-26 20:24:18 +0000

    forgot a file, and here are some new in-progress stuff on views

---
+++ trunk/system/packages/view.php
@@ -0,0 +1,15 @@
+<?php
+
+/* $Id$ */
+
+!defined('DIR_SYSTEM') && exit();
+
+/**
+ * Represents a single view.
+ * @author Peter Goodman
+ */
+class PinqView extends View implements Package {
+	public function __construct(Scope $scope) {
+		
+	}
+}
