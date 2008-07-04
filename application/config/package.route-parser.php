949dccb77797fd8e5aa996574d518730564e55bc
peter.goodman <peter.goodman@3c0eaa4a-8e4f-0410-92f7-c37bf31e9ff0>
2008-07-04 18:29:25 +0000

    ahahahah I am really hacking CSS right now

--- application/config/package.route-parser.php
+++ application/config/package.route-parser.php
@@ -2,14 +2,15 @@
 
 !defined('DIR_APPLICATION') && exit();
 
-$routes['/page/(:num)'] = '/index/index/';
-$routes['/archive'] = '/index/archive';
 $routes['/users/(:alphanum)'] = '/users/index/$1';
 
 // archives
 $routes['/(:year)'] = '/archive/$1';
 $routes['/(:year)/(:month)'] = '/archive/$1/$2';
 $routes['/(:year)/(:month)/(:day)'] = '/archive/$1/$2/$3';
+
+// tags
+$routes['/tags/(:alphanum)'] = '/tags/index/$1';
 
 // viewing single post
 $routes['/(:year)/(:month)/(:day)/([a-zA-Z0-9-]*)'] = '/posts/index/$4';
